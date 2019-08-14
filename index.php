a48bb2dec1f369cc90c52b67f2b07a6fbdf9f1d7
HoochieMan33 <0336979@gmail.com>
2019-08-14 19:47:15 +0300

    Задание 5 выполнено

--- index.php
+++ index.php
@@ -47,6 +47,10 @@ $lots = [
         'image_url' => 'img/lot-6.jpg',
     ],
 ];
+function price_opt($input) {
+    if (ceil($input) >= 1000) $output = number_format($input, 0, '', ' ') . ' ₽';
+    return $output;
+}
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -122,7 +126,7 @@ $lots = [
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?php print($value['price']) ?><b class="rub">р</b></span>
+                                <span class="lot__cost"><?php print(price_opt($value['price'])) ?></span>
                             </div>
                             <div class="lot__timer timer">
                                 12:23
