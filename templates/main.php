0c46d2ab463b0ed50ff068b97028861018df4dd3
HoochieMan33 <0336979@gmail.com>
2019-08-15 16:35:31 +0300

    Забыл скобку main.php (строка 25)

--- templates/main.php
+++ templates/main.php
@@ -22,7 +22,7 @@
                         <img src=<?php print(strip_tags($value['image_url'])) ?> width="350" height="260" alt="">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?php print(strip_tags($lot_categories[$value['category']]) ?></span>
+                        <span class="lot__category"><?php print(strip_tags($lot_categories[$value['category']])) ?></span>
                         <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?php print(strip_tags($value['name'])) ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
