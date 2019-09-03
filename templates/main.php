a97927dabafe194da1b32bb3e93ee0adeccb4ab6
HoochieMan33 <0336979@gmail.com>
2019-09-03 15:53:27 +0300

    Задание выполнено

--- templates/main.php
+++ templates/main.php
@@ -23,7 +23,7 @@
                     </div>
                     <div class="lot__info">
                         <span class="lot__category"><?php print(strip_tags($lot_categories[$value['category_id']]['name'])) ?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?php print(strip_tags($value['name'])) ?></a></h3>
+                        <h3 class="lot__title"><a class="text-link" href="lot.php?lot_id=<?php print($value['id']); ?>"><?php print(strip_tags($value['name'])) ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
